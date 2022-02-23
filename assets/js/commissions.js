dd9b9af63c01a07a52291bc484aadcf588a97a40
CatoVR <100280242+CatoVR@users.noreply.github.com>
2022-02-23 17:28:19 +0100

    Update commissions.js

--- assets/js/commissions.js
+++ assets/js/commissions.js
@@ -7,9 +7,9 @@ window.addEventListener('load', () => {
     document.getElementById("content").classList.remove("hidden");
 
     if(commissionsopen > 0){
-        document.getElementById("commissionsstatus").innerHTML = ("Commissions: OPEN --- "+commissionsopen+" FREE SLOTS");
+        document.getElementById("commissionsstatus").innerHTML = ("Commissions: span class="lime_text">OPEN</span> --- "+commissionsopen+" FREE SLOTS");
     }else{
-        document.getElementById("commissionsstatus").innerHTML = ("Commissions: CLOSED");
+        document.getElementById("commissionsstatus").innerHTML = ("Commissions: span class="red_text">CLOSED</span>");
     }
     /*
 	var prefix = url.searchParams.get('p');
