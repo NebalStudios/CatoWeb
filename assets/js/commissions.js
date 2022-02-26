cd830bd851dec523fadac9baafc16c42e84ce584
Aiden N <56486387+Nebalus@users.noreply.github.com>
2022-02-26 18:47:39 +0100

    Add files via upload

---
+++ assets/js/commissions.js
@@ -0,0 +1,53 @@
+
+var commissionsopen = 0;
+
+window.addEventListener('load', () => {
+
+    document.getElementById("nojscript").classList.add("hidden");
+    document.getElementById("webpage").classList.remove("hidden");
+
+    if(commissionsopen > 0){
+        document.getElementById("commissionsstatus").innerHTML = ('Commissions: <span class="text-lime">OPEN</span> &emsp; '+commissionsopen+' FREE SLOTS ');
+    }else{
+        document.getElementById("commissionsstatus").innerHTML = ('Commissions: <span class="text-red">CLOSED</span>');
+    }
+    /*
+	var prefix = url.searchParams.get('p');
+
+	if(prefix == null || prefix == '' || prefix.length > 6){
+		prefix = 'm!';
+	}
+    var spans = document.querySelectorAll("#result-prefix");
+    var len = spans.length; 
+    
+    for (var i = 0; i < len; i++) { 
+        spans[i].textContent = prefix; 
+    } 
+    
+    const query = url.searchParams.get('query');
+    
+    if(query != null && query != ''){
+        var content = document.getElementById("search-input");
+        content.value = query;
+        updateCommands(query);
+    }
+    */
+});
+
+const contact = [{stag:'discord', scontent:"Cato#2907", sname:"Discord Tag", sicon:"discord.webp"},
+                {stag:'telegram', scontent:"@Catoooooooooo", sname:"Telegram Tag", sicon:"telegram.webp"}];
+function showpopup(tag){
+    if(tag != null){
+        contact.forEach(data => {
+            if(data.stag == tag){
+                console.log(data.scontent);
+                document.getElementById("popup").classList.remove("hidden");
+                document.getElementById('popup-icon').src = "assets/images/reficon/"+data.sicon;
+            }
+        });
+    }
+}
+
+function hidepopup(){ 
+    document.getElementById("popup").classList.add("hidden");
+}
---
+++ assets/js/loading.js
@@ -0,0 +1,40 @@
+const social = [{squery:'discord', slink:"https://discord.com/invite/6xdB7ZvZ95", sname:"Discord", sicon:"discord.webp", sbackground:"discord-bg.jpg"},
+                {squery:'twitch', slink:"https://www.twitch.tv/catovr/", sname:"Twitch", sicon:"twitch.webp", sbackground:null},
+                {squery:'tiktok', slink:"https://www.tiktok.com/@catovrc", sname:"TikTok", sicon:"tiktok.webp", sbackground:null},
+                {squery:'instagram', slink:"https://www.instagram.com/furry_cato/", sname:"Instagram", sicon:"instagram.webp", sbackground:null},
+                {squery:'melody', slink:"https://melodybot.ga/", sname:"Melody Bot", sicon:"melody.webp", sbackground:null},
+                {squery:'donation', slink:"https://streamlabs.com/catovr/tip", sname:"Streamlabs", sicon:"streamlabs.webp", sbackground:null},
+                {squery:'commissions', slink:"https://catovr.ga/commissions", sname:"Commissions", sicon:"commissions.webp", sbackground:null}];
+const url = (new URL(document.location));
+const duration = 3500;
+window.addEventListener('load', () => {
+
+    var query = url.searchParams.get('q');
+    var succes = false;
+    if(query != null && query != ""){
+        query = query.toLowerCase();
+        social.forEach(content => {
+            if(content.squery == query){
+                const message = "Redirecting to "+content.sname+"...";
+                document.getElementById('loading-pbvalue').innerHTML = message;
+                document.title = message;
+                document.getElementById('socialicon').src = "assets/images/reficon/"+content.sicon;
+                if(content.sbackground != null){
+                    document.body.style.background = "url('assets/images/"+content.sbackground+"')"; 
+                }
+                succes = true;
+                setTimeout(function(){
+                    document.location.href = content.slink;
+                }, duration);
+            }
+        });
+    }
+    if(!succes){
+        document.getElementById('loading-pbvalue').innerHTML = "Invalid Query";
+        document.title = "Invalid Query";
+        setTimeout(function(){
+            document.location.href = "https://catovr.ga/";
+        }, duration);
+    }
+});
+
